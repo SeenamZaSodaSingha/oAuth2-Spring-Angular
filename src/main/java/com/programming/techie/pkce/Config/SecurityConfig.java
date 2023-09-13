217cb1919945be1ce06ccf71d0959465b163a23f
SeenamZaSodaSingha <thammanit.ch@gmail.com>
2023-09-13 21:41:32 +0700

    Can debug on both BE and FE, YAY!

--- src/main/java/com/programming/techie/pkce/Config/SecurityConfig.java
+++ src/main/java/com/programming/techie/pkce/Config/SecurityConfig.java
@@ -1,25 +1,38 @@
 package com.programming.techie.pkce.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    public void configure(HttpSecurity httpSecurity) throws Exception {
+@EnableWebSecurity
+// public class SecurityConfig extends WebSecurityConfiguration {
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .cors()
-                .and()
-                .csrf()
-                .disable()
-                .oauth2ResourceServer()
-                .jwt();
+                .authorizeHttpRequests(requests -> requests
+                        .anyRequest().authenticated())
+                .sessionManagement(management -> management
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .cors(withDefaults())
+                .csrf(csrf -> csrf
+                        .disable())
+                .oauth2ResourceServer(server -> server
+                        .jwt(Customizer.withDefaults()));
+        return httpSecurity.build();
     }
+
 }
--- src/main/java/com/programming/techie/pkce/Controller/Controller.java
+++ src/main/java/com/programming/techie/pkce/Controller/Controller.java
@@ -1,6 +1,7 @@
 package com.programming.techie.pkce.Controller;
 
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
